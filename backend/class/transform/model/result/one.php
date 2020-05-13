4919e40343d05c2963ea7c57ac0c429ffa1ff744
Kevin <kevin@jocoon.de>
2020-05-13 18:52:59 +0200

    disabled internal debugInfo storage in model_result_one

--- backend/class/transform/model/result/one.php
+++ backend/class/transform/model/result/one.php
@@ -14,10 +14,10 @@ class one extends \codename\core\io\transform\model\result {
     $this->model->saveLastQuery = true;
     $result = $this->doQuery($value);
 
-    $this->debugInfo = [
-      'query' => $this->model->getLastQuery(),
-      'result' => $result
-    ];
+    // $this->debugInfo = [
+    //   'query' => $this->model->getLastQuery(),
+    //   'result' => $result
+    // ];
 
     if($result && (count($result) === 1)) {
       return $result[0]; // return the result row
