74e76fbc542d90a4ac421f0a0680142334e08dc3
Kevin <kevin@jocoon.de>
2020-05-14 09:10:17 +0200

    performance optimization of transform get_arrayvalue

--- backend/class/transform/get/arrayvalue.php
+++ backend/class/transform/get/arrayvalue.php
@@ -8,6 +8,71 @@ class arrayvalue extends \codename\core\io\transform\get {
   /**
    * @inheritDoc
    */
+  public function __construct(array $config)
+  {
+    parent::__construct($config);
+
+    $this->source = $this->config['source'];
+    $this->isSource = $this->config['source'] == 'source';
+    $this->field = $this->config['field'];
+    $this->indexIsArray = is_array($this->config['index']);
+    $this->indexValue  = $this->config['index'];
+    $this->indexSource = $this->config['index']['source'] ?? null;
+    $this->indexField = $this->config['index']['field'] ?? null;
+    $this->required = isset($this->config['required']) && $this->config['required'];
+  }
+
+  /**
+   * [protected description]
+   * @var bool
+   */
+  protected $required = null;
+
+  /**
+   * [protected description]
+   * @var string
+   */
+  protected $source = null;
+
+  /**
+   * [protected description]
+   * @var bool
+   */
+  protected $isSource = null;
+
+  /**
+   * [protected description]
+   * @var string
+   */
+  protected $field = null;
+
+  /**
+   * [protected description]
+   * @var bool
+   */
+  protected $indexIsArray = null;
+
+  /**
+   * [protected description]
+   * @var array|string
+   */
+  protected $indexValue = null;
+
+  /**
+   * [protected description]
+   * @var string
+   */
+  protected $indexSource = null;
+
+  /**
+   * [protected description]
+   * @var string
+   */
+  protected $indexField = null;
+
+  /**
+   * @inheritDoc
+   */
   public function internalTransform($value)
   {
     // if(!isset($this->config['source'])) {
@@ -16,20 +81,25 @@ class arrayvalue extends \codename\core\io\transform\get {
     //   echo("</pre>");
     // }
 
-    if($this->config['source'] == 'source') {
+    if($this->isSource) {
       // special case where we need to fetch a complete array
       // and access only an index later on
-      $v = isset($this->config['field']) ? $value[$this->config['field']] : $value;
+      if($this->field) {
+        $v = $value[$this->field];
+      } else {
+        $v = $value;
+      }
+      // $v = isset($this->config['field']) ? $value[$this->config['field']] : $value;
     } else {
-      $v = $this->getValue($this->config['source'], $this->config['field'], $value);
+      $v = $this->getValue($this->source, $this->field, $value);
     }
 
-    if(is_array($this->config['index'])) {
+    if($this->indexIsArray) {
       // dynamic index
-      $index = $this->getValue($this->config['index']['source'], $this->config['index']['field'], $value);
+      $index = $this->getValue($this->indexSource, $this->indexField, $value);
 
-      if(!isset($v[$index])) {
-        if(isset($this->config['required']) && $this->config['required']) {
+      if($v[$index] ?? false) {
+        if($this->required) {
           $this->errorstack->addError('GET_ARRAYVALUE_MISSING', 0, [
             'config' => $this->config,
             'value' => $value
@@ -41,8 +111,8 @@ class arrayvalue extends \codename\core\io\transform\get {
       return $v[$index];
     } else {
 
-      if(!isset($v[$this->config['index']])) {
-        if(isset($this->config['required']) && $this->config['required']) {
+      if($v[$this->indexValue] ?? false) {
+        if($this->required) {
           $this->errorstack->addError('GET_ARRAYVALUE_MISSING', 0, [
             'config' => $this->config,
             'value' => $value
@@ -51,7 +121,7 @@ class arrayvalue extends \codename\core\io\transform\get {
         return null;
       }
 
-      return $v[$this->config['index']];
+      return $v[$this->indexValue];
     }
 
 
