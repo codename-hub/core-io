69682286d5ec8b785b825861e726cc58ebebe515
Ralf <ralf@jocoon.de>
2021-04-27 11:09:24 +0200

    fixed substr start for correctly string length

--- backend/class/transform/get/randomstring.php
+++ backend/class/transform/get/randomstring.php
@@ -11,7 +11,7 @@ class randomstring extends \codename\core\io\transform\get
     $chars = $this->config['chars'] ?? '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $length = $this->config['length'];
     // @see https://stackoverflow.com/questions/4356289/php-random-string-generator
-    return substr(str_shuffle(str_repeat($x=$chars, ceil($length/strlen($x)) )),1,$length);
+    return substr(str_shuffle(str_repeat($x=$chars, ceil($length/strlen($x)) )),0,$length);
   }
 
   /**
