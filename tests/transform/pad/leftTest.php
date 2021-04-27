30d5e27ebd24190766549313561f2725bfbcfbd5
Kevin <kevin@jocoon.de>
2021-04-27 16:44:00 +0200

    extended tests for transform pad_left and pad_right

--- tests/transform/pad/leftTest.php
+++ tests/transform/pad/leftTest.php
@@ -17,8 +17,61 @@ class leftTest extends \codename\core\io\tests\transform\abstractTransformTest
     $result = $transform->transform([
       'example_source_field'  => 'example'
     ]);
-    // Make sure it stays an array
     $this->assertEquals('   example', $result);
+  }
+
+  /**
+   * [testNullValuePadding description]
+   */
+  public function testNullValuePadding(): void {
+    $transform = $this->getTransform('pad_left', [
+      'source'  => 'source',
+      'field'   => 'example_source_field',
+      'length'  => 10,
+      'string'  => ' '
+    ]);
+    //
+    // NOTE: at the moment, str_pad using a NULL input value
+    // also pads the string, as it was an empty string.
+    //
+    $result = $transform->transform([
+      'example_source_field'  => null
+    ]);
+    $this->assertEquals('          ', $result);
+  }
+
+  /**
+   * [testEmptyStringPadding description]
+   */
+  public function testEmptyStringPadding(): void {
+    $transform = $this->getTransform('pad_left', [
+      'source'  => 'source',
+      'field'   => 'example_source_field',
+      'length'  => 10,
+      'string'  => ' '
+    ]);
+    $result = $transform->transform([
+      'example_source_field'  => ''
+    ]);
+    $this->assertEquals('          ', $result);
+  }
+
+  /**
+   * [testValueExceedsPadLength description]
+   */
+  public function testValueExceedsPadLength(): void {
+    $transform = $this->getTransform('pad_left', [
+      'source'  => 'source',
+      'field'   => 'example_source_field',
+      'length'  => 10,
+      'string'  => ' '
+    ]);
+    $result = $transform->transform([
+      'example_source_field'  => 'exampleexample'
+    ]);
+    // Make sure the strings stays the same,
+    // as we only pad, if there are characters missing
+    $this->assertEquals('exampleexample', $result);
   }
 
   /**
