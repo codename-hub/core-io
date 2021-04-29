8763a27141a948928f9dd1bbbe5710bee07fc1e9
Ralf <ralf@jocoon.de>
2021-04-29 10:51:56 +0200

    added helper deepaccess test

---
+++ tests/helper/testDeepaccess.php
@@ -0,0 +1,36 @@
+<?php
+namespace codename\core\io\tests\datasource;
+
+class testDeepaccess extends \PHPUnit\Framework\TestCase
+{
+
+  /**
+   * [testDeepaccessGet description]
+   * @return [type] [description]
+   */
+  public function testDeepaccess () {
+    $helper = new \codename\core\io\helper\deepaccess();
+    $example = [];
+
+    // set example data
+    $example = $helper->set($example, [ 'example1', 'example2' ], 'example');
+
+    $this->assertEquals([
+      'example1'  => [
+        'example2'  => 'example'
+      ],
+    ], $example);
+
+    // get example data
+    $result = $helper->get($example, [ 'example1', 'example2' ]);
+
+    $this->assertEquals('example', $result);
+
+    // get not exists key
+    $result = $helper->get($example, [ 'error1', 'error2' ]);
+
+    $this->assertNull($result);
+
+  }
+
+}
