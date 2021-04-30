257adbff14e54cf07c1ae85208cc58dc255f826c
Kevin <kevin@jocoon.de>
2021-04-30 11:19:47 +0200

    added test for target_xml and re-read via datasource_xml

---
+++ tests/target/buffered/file/xmlWriteReadTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace codename\core\tests\target\buffered\file;
+
+use codename\core\tests\target\abstractWriteReadTest;
+
+class xmlWriteReadTest extends abstractWriteReadTest {
+
+  /**
+   * @inheritDoc
+   */
+  protected function getWriteReadTargetInstance(): \codename\core\io\target
+  {
+    return new \codename\core\io\target\buffered\file\xml('xml_test', [
+      'version' => '1.0',
+      'encoding' => 'UTF-8',
+      'template_elements_path' => ['data'],
+      'mapping' => [
+        'key1' => [ 'path' => ['element'] ],
+        'key2' => [ 'path' => ['element'] ],
+        'key3' => [ 'path' => ['element'] ],
+        'key4' => [ 'path' => ['element'] ],
+      ]
+    ]);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function readTargetData(\codename\core\io\target $target): array {
+    $files = $target->getFileResultArray();
+    $this->assertCount(1, $files);
+    foreach($files as $file) {
+      $filepath = $file->get();
+      $datasource = new \codename\core\io\datasource\xml($filepath, [
+        'xpath_query' => '/data/element',
+        'xpath_mapping' => [
+          'key1' => 'key1',
+          'key2' => 'key2',
+          'key3' => 'key3',
+          'key4' => 'key4',
+        ]
+      ]);
+      $res = [];
+      foreach($datasource as $r) {
+        $res[] = $r;
+      }
+      return $res;
+    }
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function cleanupTarget(\codename\core\io\target $target): void
+  {
+    foreach($target->getFileResultArray() as $file) {
+      unlink($file->get());
+    }
+  }
+
+}
