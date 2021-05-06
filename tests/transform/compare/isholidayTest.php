1e0f075afc5f060c9daa9a03b1f76104457b4cb1
Ralf <ralf@jocoon.de>
2021-05-06 14:44:28 +0200

    completed isholiday test

--- tests/transform/compare/isholidayTest.php
+++ tests/transform/compare/isholidayTest.php
@@ -5,10 +5,129 @@ class isholidayTest extends \codename\core\io\tests\transform\abstractTransformT
 {
 
   /**
+   * [protected description]
+   * @var bool
+   */
+  protected static $initialized = false;
+
+  /**
+   * @inheritDoc
+   */
+  public static function tearDownAfterClass(): void
+  {
+    parent::tearDownAfterClass();
+    static::$initialized = false;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function tearDown(): void
+  {
+    $this->getModel('holidays')
+      ->addFilter('holidays_id', 0, '>')
+      ->delete();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function setUp(): void
+  {
+    $app = static::createApp();
+
+    // Additional overrides to get a more complete app lifecycle
+    // and allow static global app::getModel() to work correctly
+    $app->__setApp('transformmodeltest');
+    $app->__setVendor('codename');
+    $app->__setNamespace('\\codename\\core\\io\\tests\\transform\\compare');
+
+    $app->getAppstack();
+
+    // avoid re-init
+    if(static::$initialized) {
+      return;
+    }
+
+    static::$initialized = true;
+
+    static::setEnvironmentConfig([
+      'test' => [
+        'database' => [
+          // NOTE: by default, we do these tests using
+          // pure in-memory sqlite.
+          'default' => [
+            'driver' => 'sqlite',
+            'database_file' => ':memory:',
+          ],
+        ],
+        'cache' => [
+          'default' => [
+            'driver' => 'memory'
+          ]
+        ],
+        'filesystem' =>[
+          'local' => [
+            'driver' => 'local',
+          ]
+        ],
+        'log' => [
+          'default' => [
+            'driver' => 'system',
+            'data' => [
+              'name' => 'dummy'
+            ]
+          ]
+        ],
+      ]
+    ]);
+
+    static::createModel('transformtest', 'holidays', [
+      'field' => [
+        'holidays_id',
+        'holidays_created',
+        'holidays_modified',
+        'holidays_country',
+        'holidays_orderitemcommission_type',
+        'holidays_check_date',
+        'holidays_day_type',
+        'holidays_type',
+        'holidays_type_name',
+        'holidays_can_change',
+      ],
+      'primary' => [
+        'holidays_id'
+      ],
+      'datatype' => [
+        'holidays_id'                        => 'number_natural',
+        'holidays_created'                   => 'text_timestamp',
+        'holidays_modified'                  => 'text_timestamp',
+        'holidays_country'                   => 'text',
+        'holidays_orderitemcommission_type'  => 'text',
+        'holidays_check_date'                => 'text_timestamp',
+        'holidays_day_type'                  => 'number_natural',
+        'holidays_type'                      => 'boolean',
+        'holidays_type_name'                 => 'text',
+        'holidays_can_change'                => 'boolean',
+      ],
+      'connection' => 'default'
+    ], function($schema, $model, $config) {
+      return new \codename\core\io\tests\transform\compare\model\holidays([]);
+    });
+
+    static::architect('transformmodeltest', 'codename', 'test');
+  }
+
+  /**
    * Testing transforms for Erors
    */
   public function testValueValid(): void {
-    $this->markTestIncomplete('TODO: DB Check');
+    $this->getModel('holidays')->save([
+      'holidays_country'                   => 'DE',
+      'holidays_orderitemcommission_type'  => 'energy',
+      'holidays_check_date'                => '2021-04-19',
+      'holidays_can_change'                => false,
+    ]);
 
     $transform = $this->getTransform('compare_isholiday', [
       'country'   => [
@@ -26,6 +145,35 @@ class isholidayTest extends \codename\core\io\tests\transform\abstractTransformT
     ]);
     // Make sure it stays an array
     $this->assertTrue($result);
+  }
+
+  /**
+   * Testing transforms for Erors
+   */
+  public function testValueNotValid(): void {
+    $this->getModel('holidays')->save([
+      'holidays_country'                   => 'DE',
+      'holidays_orderitemcommission_type'  => 'energy',
+      'holidays_check_date'                => '2021-04-19',
+      'holidays_can_change'                => false,
+    ]);
+
+    $transform = $this->getTransform('compare_isholiday', [
+      'country'   => [
+        'source'  => 'source',
+        'field'   => 'example_country_field',
+      ],
+      'date'      => [
+        'source'  => 'source',
+        'field'   => 'example_date_field',
+      ],
+    ]);
+    $result = $transform->transform([
+      'example_country_field' => 'DE',
+      'example_date_field'    => '2021-04-20',
+    ]);
+    // Make sure it stays an array
+    $this->assertFalse($result);
   }
 
   /**
