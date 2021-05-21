6867bc8d22e190ec511cb0b6ba7a3760f72e113e
Ralf <ralf@jocoon.de>
2021-05-21 09:45:16 +0200

    added more tests for process target

--- tests/process/target/model/deleteTest.php
+++ tests/process/target/model/deleteTest.php
@@ -52,6 +52,105 @@ class deleteTest extends abstractProcessTargetModelTest {
   }
 
   /**
+   * [testProcessDeleteWithTransformValueFilter description]
+   */
+  public function testProcessDeleteWithTransformValueFilter(): void {
+    $model = $this->getModel('processmodel');
+    $this->createTestData();
+
+    // make sure the data is there
+    $this->assertEquals(3, $model->getCount());
+
+    $pipeline = new \codename\core\io\pipeline(null, [
+      'preprocess' => [
+        'delete_with_transform_filter' => [
+          'type' => 'target_model_delete',
+          'config' => [
+            'target' => 'test',
+            'filter' => [
+              [ 'field' => 'processmodel_text', 'operator' => '=', 'value' => [ 'source' => 'transform', 'field' => 'first' ] ] // transform value
+            ]
+          ]
+        ]
+      ],
+      'source' => [
+        'type' => 'arraydata'
+      ],
+      'transform' => [
+        'first' => [
+          'type'    => 'value',
+          'config'  => [
+            'value' => 'first'
+          ],
+        ],
+      ],
+      'target' => [
+        'test' => [
+          'type'  => 'model',
+          'model' => 'processmodel',
+        ]
+      ]
+    ]);
+
+    $datasource = new \codename\core\io\datasource\arraydata();
+    $datasource->setData([]);
+    $pipeline->setDatasource($datasource);
+
+    // this might/should fail?
+    $pipeline->setDryRun(false);
+    $pipeline->run();
+
+    $this->assertEquals(2, $model->getCount());
+  }
+
+  /**
+   * [testProcessDeleteWithUnsupportedTarget description]
+   */
+  public function testProcessDeleteWithUnsupportedTarget(): void {
+    $this->expectException(\codename\core\exception::class);
+    $this->expectExceptionMessage('EXCEPTION_CORE_IO_PROCESS_TARGET_MODEL_UNSUPPORTED');
+
+    $pipeline = new \codename\core\io\pipeline(null, [
+      'preprocess' => [
+        'delete_with_transform_filter' => [
+          'type' => 'target_model_delete',
+          'config' => [
+            'target' => 'test',
+            'filter' => [
+              [ 'field' => 'processmodel_text', 'operator' => '=', 'value' => [ 'source' => 'transform', 'field' => 'first' ] ] // transform value
+            ]
+          ]
+        ]
+      ],
+      'source' => [
+        'type' => 'arraydata'
+      ],
+      'transform' => [
+        'first' => [
+          'type'    => 'value',
+          'config'  => [
+            'value' => 'first'
+          ],
+        ],
+      ],
+      'target' => [
+        'test' => [
+          'type'    => 'arraydata',
+          'mapping' => []
+        ]
+      ]
+    ]);
+
+    $datasource = new \codename\core\io\datasource\arraydata();
+    $datasource->setData([]);
+    $pipeline->setDatasource($datasource);
+
+    // this might/should fail?
+    $pipeline->setDryRun(false);
+    $pipeline->run();
+  }
+
+  /**
    * Model-Deletion calls without any filters
    * must fail due to model mechanics
    */
@@ -88,6 +187,42 @@ class deleteTest extends abstractProcessTargetModelTest {
     // this might/should fail?
     $pipeline->setDryRun(false);
     $pipeline->run();
+  }
+
+  /**
+   * [testErrorsAndDryRun description]
+   */
+  public function testErrorsAndDryRun(): void {
+    $process = new helper_process_target_model_delete([]);
+
+    $process->resetErrors();
+    $this->assertEmpty($process->getErrors());
+
+    $pipeline = new \codename\core\io\pipeline(null, []);
+    $pipeline->setDryRun(true);
+
+    $process->setPipelineInstance($pipeline);
+    $this->assertTrue($process->isDryRun());
+
+  }
+
+  /**
+   * Test Spec output (simple case)
+   */
+  public function testSpecification(): void {
+    $this->expectException(\LogicException::class);
+    $this->expectExceptionMessage('Not implemented');
+
+    $process = new \codename\core\io\process\target\model\delete([]);
+    $specification = $process->getSpecification();
+  }
+
+}
+
+class helper_process_target_model_delete extends \codename\core\io\process\target\model\delete {
+
+  public function isDryRun() : bool {
+    return parent::isDryRun();
   }
 
 }
--- tests/process/target/model/updateTest.php
+++ tests/process/target/model/updateTest.php
@@ -7,7 +7,7 @@ namespace codename\core\tests\process\target\model;
 class updateTest extends abstractProcessTargetModelTest {
 
   /**
-   * [testProcessDeleteWithoutFiltersFails description]
+   * [testProcessUpdateWithConstantValueFilterAndConstantData description]
    */
   public function testProcessUpdateWithConstantValueFilterAndConstantData(): void {
     $model = $this->getModel('processmodel');
@@ -56,6 +56,177 @@ class updateTest extends abstractProcessTargetModelTest {
   }
 
   /**
+   * [testProcessUpdateWithConstantValueFilterAndOptionData description]
+   */
+  public function testProcessUpdateWithConstantValueFilterAndOptionData(): void {
+    $model = $this->getModel('processmodel');
+    $this->createTestData();
+
+    // make sure the data is there
+    $this->assertEquals(3, $model->getCount());
+
+    $pipeline = new \codename\core\io\pipeline(null, [
+      'preprocess' => [
+        'update_with_constant_filter' => [
+          'type' => 'target_model_update',
+          'config' => [
+            'target' => 'test',
+            'filter' => [
+              [ 'field' => 'processmodel_text', 'operator' => '=', 'value' => 'second' ] // Constant value
+            ],
+            'data' => [
+              'processmodel_text' => [ 'source' => 'option', 'field' => 'updated' ]
+            ]
+          ]
+        ]
+      ],
+      'source' => [
+        'type' => 'arraydata'
+      ],
+      'transform' => [],
+      'target' => [
+        'test' => [
+          'type'  => 'model',
+          'model' => 'processmodel',
+        ]
+      ]
+    ]);
+    $pipeline->setOptions([
+      'updated' => 'updated'
+    ]);
+
+    $datasource = new \codename\core\io\datasource\arraydata();
+    $datasource->setData([]);
+    $pipeline->setDatasource($datasource);
+
+    // this might/should fail?
+    $pipeline->setDryRun(false);
+    $pipeline->run();
+
+    $res = $model->addFilter('processmodel_text', 'updated')->search()->getResult();
+    $this->assertCount(1, $res);
+  }
+
+  /**
+   * [testProcessUpdateWithTransformValueFilterAndTransformData description]
+   */
+  public function testProcessUpdateWithTransformValueFilterAndTransformData(): void {
+    $model = $this->getModel('processmodel');
+    $this->createTestData();
+
+    // make sure the data is there
+    $this->assertEquals(3, $model->getCount());
+
+    $pipeline = new \codename\core\io\pipeline(null, [
+      'preprocess' => [
+        'update_with_transform_filter' => [
+          'type' => 'target_model_update',
+          'config' => [
+            'target' => 'test',
+            'filter' => [
+              [ 'field' => 'processmodel_text', 'operator' => '=', 'value' => [ 'source' => 'transform', 'field' => 'first' ] ] // transform value
+            ],
+            'data' => [
+              'processmodel_text' => [ 'source' => 'transform', 'field' => 'updated' ]
+            ]
+          ]
+        ]
+      ],
+      'source' => [
+        'type' => 'arraydata'
+      ],
+      'transform' => [
+        'first' => [
+          'type'    => 'value',
+          'config'  => [
+            'value' => 'first'
+          ],
+        ],
+        'updated' => [
+          'type'    => 'value',
+          'config'  => [
+            'value' => 'updated'
+          ],
+        ],
+      ],
+      'target' => [
+        'test' => [
+          'type'  => 'model',
+          'model' => 'processmodel',
+        ]
+      ]
+    ]);
+
+    $datasource = new \codename\core\io\datasource\arraydata();
+    $datasource->setData([]);
+    $pipeline->setDatasource($datasource);
+
+    // this might/should fail?
+    $pipeline->setDryRun(false);
+    $pipeline->run();
+
+    $res = $model->addFilter('processmodel_text', 'updated')->search()->getResult();
+    $this->assertCount(1, $res);
+  }
+
+  /**
+   * [testProcessUpdateWithUnsupportedTarget description]
+   */
+  public function testProcessUpdateWithUnsupportedTarget(): void {
+    $this->expectException(\codename\core\exception::class);
+    $this->expectExceptionMessage('EXCEPTION_CORE_IO_PROCESS_TARGET_MODEL_UNSUPPORTED');
+
+    $pipeline = new \codename\core\io\pipeline(null, [
+      'preprocess' => [
+        'update_with_transform_filter' => [
+          'type' => 'target_model_update',
+          'config' => [
+            'target' => 'test',
+            'filter' => [
+              [ 'field' => 'processmodel_text', 'operator' => '=', 'value' => [ 'source' => 'transform', 'field' => 'first' ] ] // transform value
+            ],
+            'data' => [
+              'processmodel_text' => [ 'source' => 'transform', 'field' => 'updated' ]
+            ]
+          ]
+        ]
+      ],
+      'source' => [
+        'type' => 'arraydata'
+      ],
+      'transform' => [
+        'first' => [
+          'type'    => 'value',
+          'config'  => [
+            'value' => 'first'
+          ],
+        ],
+        'updated' => [
+          'type'    => 'value',
+          'config'  => [
+            'value' => 'updated'
+          ],
+        ],
+      ],
+      'target' => [
+        'test' => [
+          'type'    => 'arraydata',
+          'mapping' => [],
+        ],
+      ]
+    ]);
+
+    $datasource = new \codename\core\io\datasource\arraydata();
+    $datasource->setData([]);
+    $pipeline->setDatasource($datasource);
+
+    // this might/should fail?
+    $pipeline->setDryRun(false);
+    $pipeline->run();
+  }
+
+
+  /**
    * Model-Deletion calls without any filters
    * must fail due to model mechanics
    */
@@ -95,6 +266,42 @@ class updateTest extends abstractProcessTargetModelTest {
     // this might/should fail?
     $pipeline->setDryRun(false);
     $pipeline->run();
+  }
+
+  /**
+   * [testErrorsAndDryRun description]
+   */
+  public function testErrorsAndDryRun(): void {
+    $process = new helper_process_target_model_update([]);
+
+    $process->resetErrors();
+    $this->assertEmpty($process->getErrors());
+
+    $pipeline = new \codename\core\io\pipeline(null, []);
+    $pipeline->setDryRun(true);
+
+    $process->setPipelineInstance($pipeline);
+    $this->assertTrue($process->isDryRun());
+
+  }
+
+  /**
+   * Test Spec output (simple case)
+   */
+  public function testSpecification(): void {
+    $this->expectException(\LogicException::class);
+    $this->expectExceptionMessage('Not implemented');
+
+    $process = new \codename\core\io\process\target\model\update([]);
+    $specification = $process->getSpecification();
+  }
+
+}
+
+class helper_process_target_model_update extends \codename\core\io\process\target\model\update {
+
+  public function isDryRun() : bool {
+    return parent::isDryRun();
   }
 
 }
